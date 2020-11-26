c724cd37e805814675d8cefd73beb63539d93964
Chad-AquaQ <chad.thackray@aquaq.co.uk>
2020-11-26 17:24:37 +0000

    changed to now use default port, and only setup_dashboards.sh for troubleshooting

--- setup_dashboards.sh
+++ setup_dashboards.sh
@@ -2,35 +2,22 @@
 
 if [[ $# -eq 0 ]]
 then
-	echo "Usage:"
-	echo "Please provide the baseport of your TorQ-Air stack as the first argument"
-	echo "And the path to your Kx Dashboards 'dash' folder as the second"
+	echo "Usage: Please provide the baseport of your TorQ-Air stack as an argument"
 	exit 0
 fi
 
 baseport=$1
-dashFolder=$2
 
 RDBPORT=$(($baseport + 2))
 HDBPORT=$(($baseport + 3))
 BOARDPORT=$(($baseport + 10))
 
-declare -a connections=("rdb.json" "boards.json")
-declare -a dashboards=("slide1.json" "slide2.json" "slide3.json" "slide4.json")
+declare -a dashboards=("boards.json" "rdb.json" "slide1.json" "slide2.json" "slide3.json" "slide4.json")
 
-all=( "${dashboards[@]}" "${connections[@]}" )
-
-# Replaces placeholder port numbers in all files in /dashboards
-for item in "${all[@]}"
+# Replaces default port numbers in all files in /dashboards
+for item in "${dashboards[@]}"
 do
-	sed -i s/RDBPORT/$RDBPORT/g dashboards/$item
-	sed -i s/HDBPORT/$HDBPORT/g dashboards/$item 
-	sed -i s/BOARDPORT/$BOARDPORT/g dashboards/$item 
+	sed -i s/24027/$RDBPORT/g dashboards/$item
+	sed -i s/24028/$HDBPORT/g dashboards/$item 
+	sed -i s/24035/$BOARDPORT/g dashboards/$item 
 done
-
-# Moves rdb.json and boards.json to the connections folder in Kx dashboards
-for conn in "${connections[@]}"
-do
-	cp dashboards/$conn $dashFolder/data/connections
-done
-
