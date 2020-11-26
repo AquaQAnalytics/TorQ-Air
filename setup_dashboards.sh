89b847e70dd9ff68f4ab88dd12d5964288f64c27
Chad-AquaQ <chad.thackray@aquaq.co.uk>
2020-11-26 17:28:16 +0000

    Added file checking before executing sed

--- setup_dashboards.sh
+++ setup_dashboards.sh
@@ -17,6 +17,12 @@ declare -a dashboards=("boards.json" "rdb.json" "slide1.json" "slide2.json" "sli
 # Replaces default port numbers in all files in /dashboards
 for item in "${dashboards[@]}"
 do
+	if [ ! -f dashboards/$item ]
+	then
+		echo "$item not found, exiting program"
+		echo "Ensure you are running this from the TorQ root folder"
+		exit 1
+	fi
 	sed -i s/24027/$RDBPORT/g dashboards/$item
 	sed -i s/24028/$HDBPORT/g dashboards/$item 
 	sed -i s/24035/$BOARDPORT/g dashboards/$item 
